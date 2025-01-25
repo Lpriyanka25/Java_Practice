df390725405da8ddcf6ddf11cb9d0b7ffc6421f4
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2025-01-25 22:51:56 +0530

    Add files via upload

--- Program.java
+++ Program.java
@@ -1,40 +1,19 @@
-import java.util.Scanner;
+package com.sunbeam;
 
-class Employee
-{
-	//Fields 
-	private int empid; //Instance variable 
-	private String name; //Instance variable 
-	private float salary; //Instance Variable 
-	//Methods 
-	public void acceptRecord( )
-	{
-		Scanner sc = new Scanner(System.in) ;
-		System.out.println("Name : ");
-		name = sc.nextLine(); 
-		System.out.println("Empid : ");
-		empid = sc.nextInt(); 
-		System.out.println("Salary : ");
-		salary = sc.nextFloat(); 
-	}
-	public void printRecord( )
-	{
-		System.out.println("Name : "+name);
-		System.out.println("Empid : "+empid);
-		System.out.println("Salary : "+salary);
-	}
-}
 public class Program {
-	
+
 	public static void main(String[] args) {
-		Employee emp = new Employee(); 
-		Employee emp2 = new Employee(); 
-		Employee emp3 = new Employee(); 
+		System.out.println("Hello world");
+		Program.main(10, 20);
+		Program.main();
+
 	}
-	public static void main1(String[] args) {
-		Employee emp = new Employee(); 
-		emp.acceptRecord(); 
-		emp.printRecord(); //message passing 
+	public static void main(int a, int b) {
+		System.out.println("Hello world1");
+
+	}
+	public static void main() {
+		System.out.println("Hello world2");
 
 	}
 
