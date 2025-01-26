53fa675a40c351a0dfffdf2d770e72f9c60e9d7e
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2025-01-26 23:06:46 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,18 @@
+package com.sunbeam;
+
+public class Program {
+
+	public static void main(String[] args) {
+		//variable declaration 
+		double lenght = 10.00 , breadth = 2.00 , area; 
+		//Calculating the area
+		area = lenght * breadth; 
+		//printing the area 
+		System.out.println("Area " + area);
+		System.out.println("Length  " + lenght + "breadth " + breadth + "area " + area);
+		System.out.printf("Area = %f",area);
+		System.out.println();
+		System.out.println("Length "  + lenght);
+	}
+
+}
