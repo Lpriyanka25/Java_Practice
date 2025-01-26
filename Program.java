1cf5b8589929543bf03d6c037b9836158157c721
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2025-01-26 23:09:56 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,24 @@
+package com.sunbeam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+		int a = 10 , b = 20; 
+		
+		int max = Integer.max(a, b); 
+		//System.out.println("Max " + max);
+		
+		System.out.println("Max value " + Integer.MAX_VALUE);
+		
+		List<Integer>list = new ArrayList<Integer>( ); 
+		list.add(10); 
+		list.add(20); 
+		list.add(30); 
+		list.add(40); 
+	}
+	
+
+}
