64857b48e30b78237e63d848bc03bc51961bdc57
Priyanka Langote <161610464+Lpriyanka25@users.noreply.github.com>
2025-01-27 22:53:10 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,61 @@
+package com.sunbeam;
+
+import java.util.Scanner;
+
+public class Program {
+
+	/* 
+	public static void main(String[] args) { // ( name , accountnumber , balance)
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name :: ");
+		String name = sc.next();
+		System.out.println("Enter the accountNumber");
+		int accountNumber = sc.nextInt();
+		System.out.println("Enter the Balance : ");
+		double balance = sc.nextDouble();
+
+		System.out.println("Name : " + name);
+		System.out.println("AccountNumber : " + accountNumber);
+		System.out.println("Balance : " + balance);
+
+	}
+	*/  
+	/*
+	 * public static void main(String[] args) { //( name , accountnumber , balance)
+	 * Scanner sc = new Scanner(System.in); System.out.print("Enter the name :: ");
+	 * String name = sc.nextLine(); System.out.print("Enter the accountNumber"); int
+	 * accountNumber= sc.nextInt(); System.out.print("Enter the Balance : "); double
+	 * balance = sc.nextDouble();
+	 * 
+	 * System.out.println("Name : "+name);
+	 * System.out.println("AccountNumber : "+accountNumber);
+	 * System.out.println("Balance : "+balance);
+	 * 
+	 * 
+	 * 
+	 * 
+	 * }
+	 */
+	public static void main(String[] args) {
+		//( name , accountnumber , balance)  
+		Scanner sc = new Scanner(System.in); 
+		
+		System.out.print("Enter the accountNumber :: ");
+		int accountNumber= sc.nextInt(); 
+		
+		System.out.print("Enter the name :: ");
+		sc.nextLine(); 
+		String name = sc.nextLine();  
+		
+		System.out.print("Enter the Balance : ");
+		double balance = sc.nextDouble(); 
+		
+		System.out.println("Name : "+name);
+		System.out.println("AccountNumber : "+accountNumber);
+		System.out.println("Balance : "+balance);
+				
+				
+	} 
+
+
+}
